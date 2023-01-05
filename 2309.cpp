4f6f157fafd12964e67053f0b45b56fcea7b4b3e
YongBon Jeon <bon6143@gmail.com>
2023-01-06 04:02:39 +0900

    recursion - combination

---
+++ 2309.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> input;
+
+void combination(int start, vector<int> v)
+{
+    //cout << "z" << v.size() <<"\n";
+    if(v.size() == 7)
+    {
+        if(accumulate(v.begin(),v.end(),0) == 100)
+        {
+            for(int i = 0 ; i < 7 ; i++)
+                printf("%d\n",v[i]);
+            //cout << accumulate(v.begin(),v.end(), 0) << "\n";
+            exit(0);
+        }
+    }
+
+    for(int i = start+1 ; i < 9 ; i++)
+    {
+        v.push_back(input[i]);
+        combination(i, v);
+        v.pop_back();
+    }
+}
+
+int main()
+{
+    vector<int> v;
+    for(int i = 0 ; i < 9 ; i++){
+        int tmp;
+        scanf("%d",&tmp);
+        input.push_back(tmp);
+    }
+    
+    sort(input.begin(), input.end());
+
+    combination(-1,v);
+}
