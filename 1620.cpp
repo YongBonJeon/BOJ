0200a3161914b7159b4017cec64deb76f8c6f4cd
YongBon Jeon <bon6143@gmail.com>
2023-01-08 01:04:03 +0900

    bidirectional map

--- 1620.cpp
+++ 1620.cpp
@@ -1,7 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+    string -> Int = map
+    Int -> string = array
+*/
+
 int main()
 {
+    ios::ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N, M;
+    map<string,int> m1;
+    map<int,string> m2;
+    string m3[100004];
+
+    cin >> N >> M;
+
+    for(int i = 0 ; i < N ; i++)
+    {
+        string tmp;
+        cin >> tmp;
+        m1[tmp] = i+1;
+        m2[i+1] = tmp;
+        m3[i+1] = tmp;
+    }
+
+    for(int i = 0 ; i < M ; i++)
+    {
+        string tmp;
+        cin >> tmp;
+
+        if(atoi(tmp.c_str()) == 0)
+            cout << m1[tmp] << "\n";
+        else
+            cout << m3[atoi(tmp.c_str())] << "\n";
+            //cout << m2[atoi(tmp.c_str())] << "\n"; 
+
+    }
     
 }
