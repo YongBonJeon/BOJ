8a4783e3fd5cf043423c551e72545f4754b4ff77
YongBon Jeon <bon6143@gmail.com>
2023-01-07 22:58:32 +0900

    Sliding window

---
+++ 2559.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N,K;
+    vector<int> input;
+    int sum  = 0, max;
+
+    scanf("%d %d",&N,&K);
+    
+    for(int i = 0 ; i < N ; i++)
+    {
+        int tmp;
+        scanf("%d",&tmp);
+
+        input.push_back(tmp);
+    }
+
+    for(int i = 0 ; i < K ; i++)
+        sum += input[i];
+    max = sum;
+    
+    for(int i = 0 ; i < N-K ; i++)
+    {
+        sum -= input[i];
+        sum += input[i+K];
+
+        if(sum > max)
+            max = sum;
+    }
+    printf("%d\n",max);
+
+}
