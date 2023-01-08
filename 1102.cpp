3573d8782b5664ef1698649cba1320a5687197a4
YongBon Jeon <bon6143@gmail.com>
2023-01-09 03:47:43 +0900

    Connected component(map)

---
+++ 1102.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N,M,K;
+int m[51][51];
+int visited[51][51];
+int dy[4] = {-1,0,1,0};
+int dx[4] = {0,1,0,-1};
+
+void dfs(int y, int x)
+{
+    int ny, nx;
+
+    visited[y][x] = 1;
+
+    for(int i = 0 ; i < 4 ; i++)
+    {
+        ny = y + dy[i];
+        nx = x + dx[i];
+
+        if(ny < 0 || ny >= M || nx < 0 || nx >= N || !m[ny][nx])
+            continue;
+        
+        if(!visited[ny][nx])
+            dfs(ny, nx);
+    }
+}
+
+int main()
+{
+    int num_TC;
+
+    scanf("%d", &num_TC);
+
+    for(int i = 0 ; i < num_TC ; i++)
+    {
+        int ans = 0;
+        scanf("%d %d %d",&N, &M, &K);
+        memset(m, 0, sizeof(m));
+        memset(visited, 0, sizeof(visited));
+
+        for(int j = 0 ; j < K ; j++)
+        {
+            int y,x;
+            scanf("%d %d",&x, &y);
+            m[y][x] = 1;
+        }
+
+        for(int p = 0 ; p < M ; p++)
+        {
+            for(int q = 0 ; q < N ; q++)
+            {
+                if(m[p][q] && !visited[p][q])
+                {
+                    /*for(int z = 0 ; z < M ; z++){
+                        for(int c = 0 ; c < N ; c++)
+                            printf("%d ",m[z][c]);
+                        printf("\n");
+                    }
+                    for(int z = 0 ; z < M ; z++){
+                        for(int c = 0 ; c < N ; c++)
+                            printf("%d ",visited[z][c]);
+                        printf("\n");
+                    }
+                    
+                    printf("_%d %d\n", p, q);
+                    
+                    */
+                    dfs(p,q);
+                    ans++;
+                }
+            }
+        }
+        printf("%d\n",ans);
+    }
+
+}
