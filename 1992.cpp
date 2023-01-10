c12a8a180437cbc7fc8a9357a1236e6389fbd97a
YongBon Jeon <bon6143@gmail.com>
2023-01-11 00:44:17 +0900

    Divide and Conquer

--- 1992.cpp
+++ 1992.cpp
@@ -7,30 +7,34 @@ int m[65][65];
 int check_area(int y, int x, int n)
 {
     int s = m[y][x];
-    for(int i = y ; i < n ; i++)
+    //printf("s %d\n",s);
+    for(int i = y ; i < y+n ; i++)
     {
-        for(int j = x ; j < n ; j++)
+        for(int j = x ; j < x+n ; j++)
         {
+            //printf("%d ",m[i][j]);
             if(m[i][j] != s)
                 return -1;
         }
     }
+    //printf("\n");
     return s;
 }
 
 void recursion(int y, int x, int n)
 {
     int rst;
+    //printf("%d %d %d\n",y,x,n);
     if((rst = check_area(y,x,n)) >= 0)
     {
         printf("%d",rst);
     }
     else{
         printf("(");
-        recursion(x, x, n/2);
-        recursion(x, x+n/2 , n/2);
-        recursion(x+n/2,x,n/2);
-        recursion(x+n/2,x+n/2,n/2);
+        recursion(y, x, n/2);
+        recursion(y, x+n/2 , n/2);
+        recursion(y+n/2,x,n/2);
+        recursion(y+n/2,x+n/2,n/2);
         printf(")");
     }
     
@@ -45,5 +49,6 @@ int main()
             scanf("%1d",&m[i][j]);
 
     recursion(0,0,N);
+    printf("\n");
     
 }
