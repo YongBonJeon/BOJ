f632b8c6d2e9b06943c88fab8ea533866ff77815
YongBon Jeon <bon6143@gmail.com>
2023-01-12 21:17:45 +0900

    simple implement

---
+++ 10709.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int H,W;
+char m[101][101];
+int ans[101][101];
+
+void solve(int h)
+{
+    int st;
+    for(int i = W-1 ; i >= 0 ; i--)
+    {
+        if(m[h][i] == 'c')
+        {
+            //printf("h %d %d\n",h,i);
+            st = 1;
+
+            while(m[h][i+st] != 'c' && i+st < W)
+            {
+                //printf("%d %d\n",h,i+st);
+                ans[h][i+st] = st;
+                st++;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    cin >> H >> W;
+
+    for(int i = 0 ; i < H ; i++)
+        for(int j = 0 ; j < W ; j++){
+            cin >> m[i][j];
+            if(m[i][j] == 'c')
+                ans[i][j] = 0;
+            else
+                ans[i][j] = -1;
+        }
+    for(int i = 0 ; i < H ; i++)
+        solve(i);
+
+    for(int i = 0 ; i < H ; i++)
+    {
+        for(int j = 0 ; j < W ; j++)
+        {
+            printf("%d ",ans[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+}
