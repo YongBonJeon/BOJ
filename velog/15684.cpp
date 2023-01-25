d07ebbc955642e6d34e6fb4826d87f2429ef14e6
YongBon Jeon <bon6143@gmail.com>
2023-01-25 10:37:58 +0900

    back-tracking

--- velog/15684.cpp
+++ velog/15684.cpp
@@ -2,114 +2,75 @@
 using namespace std;
 
 int N,M,H;
-int min_ = 9999;
+int m[31][11];
+int min_ = 99;
 
-vector<pair<int,int>> path[31];
-
-int go(int v,int level)
+bool game()
 {
-    if(level == H+1)
-        return v;
-
-    for(auto x : path[level])
+    for(int i = 1 ; i <= N ; i++)
     {
-        if(x.first == v)
-        {
-            return go(x.second, level+1);;
-        }
-    }
-    return go(v,level+1);
-}
+        int v = i;
 
-void add_ladder(int level, int num)
-{
-    if(level > H)
-        return ;
-
-    if(num <= 3)
-    {
-        int success = 1;
-        for(int i = 1 ; i <= N ; i++)
+        for(int level = 1 ; level <= H ; level++)
         {
-            int temp;
-            if((temp = go(i,0)) != i){
-                success--;
-                break;
+            if(m[level][v] == 1)
+            {
+                v++;
+            }
+            else if(m[level][v-1] == 1)
+            {
+                v--;
             }
         }
-        if(success){
-            //printf("%d %d\n",path[H].back().first, path[H].back().second);
-            if(min_ > num)
-                min_ = num;
-            return ;
-        }
+        //printf("%d %d\n",i,v);
+        if(v != i)
+            return false; 
+    }
+    return true;
+}
+
+void add_ladder(int cur, int num)
+{
+    if(num <= 3)
+    {
+        if(game())
+            min_ = min(min_, num);
     }
     else
         return ;
 
-    /*for(int i = 1 ; i <= H ; i++)
+    for(int level = cur ; level <= H ; level++)
     {
-        for(int j = 1 ; j < N ; j++)
+        for(int i = 1 ; i < N ; i++)
         {
-            bool add = 1;
-            for(auto x : path[i])
-            {
-                if(x.first == i)
-                    add = 0;
-                if(x.first == i-1)
-                    add = 0;
-            }
-            if(add)
-            {
-                path[i].push_back({i,i+1});
-                path[i].push_back({i+1,i});
-                add_ladder(level, num+1);
-                path[level].pop_back();
-                path[level].pop_back();
-            }
-        }
-    }*/
+            if(m[level][i] == 1)
+                continue;
+            if(m[level][i+1] == 1 || m[level][i-1] == 1)
+                continue;
 
-    for(int i = 1 ; i < N ; i++)
-    {
-        bool add = 1;
-        for(auto x : path[level])
-        {
-            if(x.first == i)
-                add = 0;
-            if(x.first == i-1)
-                add = 0;
-            if(x.first == i+1)
-                add = 0;
-        }
-        if(add)
-        {
-            path[level].push_back({i,i+1});
-            path[level].push_back({i+1,i});
+            m[level][i] = 1;
             add_ladder(level, num+1);
-            path[level].pop_back();
-            path[level].pop_back();
+            m[level][i] = 0;
         }
     }
-    add_ladder(level+1, num);
 }
 
 int main()
 {
-    scanf("%d %d %d",&N, &M, &H);
+    scanf("%d %d %d",&N,&M,&H);
 
     int level, v;
     for(int i = 0 ; i < M ; i++)
     {
         scanf("%d %d",&level, &v);
-        path[level].push_back({v,v+1});
-        path[level].push_back({v+1,v});
+
+        m[level][v] = 1;
     }
-
+    
     add_ladder(1,0);
-    if(min_ != 9999)
-        printf("%d\n",min_);
-    else
-        printf("-1\n");
 
+    if(min_ > 3)
+        printf("-1\n");
+    else
+        printf("%d\n",min_);
 }
