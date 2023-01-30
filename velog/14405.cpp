457c68dafd76f2bcda15fb0c1506aa3031116770
YongBon Jeon <bon6143@gmail.com>
2023-01-30 10:42:31 +0900

    implement

---
+++ velog/14405.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string ex[3] = 
+    { 
+        "pi",
+        "ka",
+        "chu"
+    };
+string s;
+int N;
+bool success = 0;
+
+void make(string x)
+{
+    //cout << x << '\n';
+    if(x.length() >= N)
+    {
+        if(!x.compare(s)){
+            //cout << "sibal" <<'\n';
+            success = 1;
+        }
+        return ;
+    }
+
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        //cout << x.length() << " " << s[x.length()] << '\n';
+        if(s[x.length()] == ex[i][0])
+            make(x+ex[i]);
+    }
+}
+
+int main()
+{
+    cin >> s;
+
+    N = s.length();
+
+    string temp;
+    make(temp);
+
+    if(success)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+}
