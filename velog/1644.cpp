196672d09020f11440a9dee7eb9f206c4a3043a0
YongBon Jeon <bon6143@gmail.com>
2023-02-04 17:09:00 +0900

    two pointer(hard)

---
+++ velog/1644.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool prime[4000001];
+int N;
+
+int main()
+{
+    scanf("%d",&N);
+
+    for(int i = 2 ; i <= N ; i++)
+        prime[i] = 1;
+
+    for(int i = 2 ; i <= N ; i++)
+    {
+        if(!prime[i])
+            continue;
+        for(int j = i+i ; j <= N ; j += i)
+            prime[j] = 0;
+    }
+    /*for(int i = 2 ; i <= N ; i++)
+        if(prime[i])
+            printf("%d\n",i);    */
+    
+    int l = 2, r = 3;
+    int sum_prime = l+r, ans = 0;
+
+    while(l <= N && r <= N)
+    {
+        //printf("%d %d %d\n",l,r,sum_prime);
+        if(sum_prime < N)
+        {
+            r++;
+            while(!prime[r])
+                r++;
+            
+            sum_prime += r;
+        }
+        else if(sum_prime > N)
+        {
+            sum_prime -= l;
+            
+            l++;
+            while(!prime[l])
+                l++;
+        }
+        else if(sum_prime == N)
+        {
+            sum_prime -= l;
+            
+            l++;
+            while(!prime[l])
+                l++;
+            //printf("ans ");
+            ans++;
+        }
+    }
+    if(N == 2)
+        ans++;
+    printf("%d\n",ans);
+}
