7376ae372529cfe5cdd5723493113f0409b61a65
YongBon Jeon <bon6143@gmail.com>
2023-02-15 21:08:29 +0900

    stack 좋은 문제!

--- velog/15926.cpp
+++ velog/15926.cpp
@@ -10,18 +10,13 @@ int main()
 {
     cin >> N >> input;
 
-    for(int i = 0 ; i < N ; i++)
-    {
+    for(int i = 0 ; i < N ; i++){
         if(input[i] == '(')
-        {
             s.push(i);
-        }
         else if(input[i] == ')')
         {
             if(s.empty())
-            {
                 continue;
-            }
             check[i] = check[s.top()] = 1;
             s.pop();
         }
@@ -30,7 +25,6 @@ int main()
     int local = 0, global = 0;
     for(int i = 0 ; i < N ; i++)
     {
-        //printf("%d ",check[i]);
         if(check[i])
             local++;
         else
@@ -41,5 +35,5 @@ int main()
     }
     global = max(global, local);
 
-    printf("%d\n",global);
+    cout << global << '\n';
 }
