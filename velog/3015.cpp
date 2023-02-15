a780cfb55a1f9d1f47ec6718e6cec9893a7bce74
YongBon Jeon <bon6143@gmail.com>
2023-02-16 01:21:15 +0900

    짝짓기 stack 활용 최고난이도

--- velog/3015.cpp
+++ velog/3015.cpp
@@ -3,37 +3,31 @@ using namespace std;
 
 int N;
 stack<pair<int, int>> s;
-int check[500001];
 
 int main()
 {
     scanf("%d",&N);
 
-    int t,temp,ans = 0;
+    int t,cnt,ans = 0;
     for(int i = 0 ; i < N ; i++)
     {
         scanf("%d",&t);
 
-        if(s.empty())
-        {
-            s.push({t,1});
-        }
-        else
-        {
-            if(s.top().first < t)
-            {
-                temp = s.top().second;
-                s.pop();
-                if(!s.empty())
-                    s.top().second += temp;
-                s.push({t,1});
-                ans += temp;
+        cnt = 1;
+        while(!s.empty() && s.top().first <= t){
+            ans += s.top().second;
+
+            if(s.top().first == t){
+                cnt = s.top().second + 1;
             }
-            else if(s.top().first > t)
-            {
-                s.top().second++;
-                s.push({t,1});
-            }
+            else
+                cnt = 1;
+            s.pop();
         }
+        if(!s.empty())
+            ans++;
+        s.push({t,cnt});
     }
+    printf("%d\n",ans);
+
 }
