257a529a4ea7b6ffead44ab20080bfffab6b9223
YongBon Jeon <bon6143@gmail.com>
2023-02-19 01:55:28 +0900

    BFS + 시뮬레이션

--- velog/16234.cpp
+++ velog/16234.cpp
@@ -16,22 +16,18 @@ void select_union(int y, int x, int num)
     q.push({y,x});
     visited[y][x] = num;
 
-    while(!q.empty())
-    {
+    while(!q.empty()){
         tie(y,x) = q.front(); q.pop();
 
-        for(int i = 0 ; i < 4 ; i++)
-        {
+        for(int i = 0 ; i < 4 ; i++){
             ny = y + dy[i];
             nx = x + dx[i];
 
             if(ny < 0 || nx < 0 || ny >= N || nx >= N)
                 continue;
 
-            if(visited[ny][nx] != num)
-            {
-                if(abs(A[ny][nx]-A[y][x]) >= L && abs(A[ny][nx]-A[y][x]) <= R)
-                {
+            if(visited[ny][nx] != num){
+                if(abs(A[ny][nx]-A[y][x]) >= L && abs(A[ny][nx]-A[y][x]) <= R){
                     visited[ny][nx] = num;
                     count++;
                     q.push({ny, nx});
@@ -50,19 +46,16 @@ void move(int y, int x)
     for(int i = 0 ; i < N ; i++)
     {
         for(int j = 0 ; j < N ; j++)
-            if(visited[i][j] == num)
-            {
+            if(visited[i][j] == num){
                 total += A[i][j];
                 count++;
             }
     }
     int move = total/count;
 
-    for(int i = 0 ; i < N ; i++)
-    {
+    for(int i = 0 ; i < N ; i++){
         for(int j = 0 ; j < N ; j++)
-            if(visited[i][j] == num)
-            {
+            if(visited[i][j] == num){
                 A[i][j] = move;
                 visited[i][j] = 0;
             }
@@ -71,90 +64,41 @@ void move(int y, int x)
 
 int main()
 {
-    int final[51][51];
     scanf("%d %d %d",&N, &L, &R);
 
     for(int i = 0 ; i < N ; i++)
-    {
         for(int j = 0 ; j < N ; j++)
-        {
             scanf("%d",&A[i][j]);
-            final[i][j] = i*N+j+1;
-        }
-    }
-
 
 
     int ans = 0;
     while(1)
     {
-        //printf("%d\n\n",ans);
         int num = 1;
+
+        // 연합 찾기
         for(int i = 0 ; i < N ; i++)
-        {
             for(int j = 0 ; j < N ; j++)
-            {
                 if(!visited[i][j])
                     select_union(i,j, num++);
-            
-        /*for(int i = 0 ; i < N ; i++)
-        {
-            for(int j = 0 ; j < N ; j++)
-            {
-                printf("%d ",visited[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");*/
-            }
-        }
 
+        // 종료 조건 확인
         int fin = 0;
         for(int i = 0 ; i < N ; i++)
-        {
             for(int j = 0 ; j < N ; j++)
-            {
                 if(visited[i][j] == 0)
                     fin++;
-            }
-        }
-    //printf("fin %d\n",fin);
         if(fin == N*N)
             break;
 
-
-
-        for(int i = 0 ; i < N ; i++)
-        {
-            for(int j = 0 ; j < N ; j++)
-            {
+        // 인구 이동
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
                 if(visited[i][j] > 0)
                     move(i,j);
-            
-
-        /*for(int i = 0 ; i < N ; i++)
-        {
-            for(int j = 0 ; j < N ; j++)
-            {
-                printf("%d ",A[i][j]);
             }
-            printf("\n");
         }
-        printf("\n");
-            for(int i = 0 ; i < N ; i++)
-        {
-            for(int j = 0 ; j < N ; j++)
-            {
-                printf("%d ",visited[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");*/
-        }
-        }
-
         ans++;
     }
-    
     printf("%d\n",ans);
 }
