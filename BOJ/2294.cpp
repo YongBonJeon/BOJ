bd5c0e15a72d9355c27bd1ad989675f6403d9101
YongBon Jeon <bon6143@gmail.com>
2023-03-08 22:48:13 +0900

    리팩터링

--- BOJ/2294.cpp
+++ BOJ/2294.cpp
@@ -3,14 +3,25 @@ using namespace std;
 
 int n,k;
 int dp[100001];
+int INF = 98765321;
 
 int main(){
     scanf("%d %d",&n, &k);
 
-    for(int i = 0 ; i <= k ; i++)
-        dp[i] = INT_MAX;
+    for(int i = 1 ; i <= k ; i++)
+        dp[i] = INF;
 
     int t;
+    for(int i = 0 ; i < n ; i++){
+        scanf("%d",&t);
+
+        for(int j = t ; j <= k ; j++){
+            dp[j] = min(dp[j], dp[j-t]+1);
+        }
+
+    }
+
+    /*int t;
     for(int i = 0 ; i < n ; i++){
         scanf("%d",&t);
         dp[t] = 1;
@@ -21,9 +32,9 @@ int main(){
             if(dp[j] != INT_MAX && dp[i-j] != INT_MAX)
                 dp[i] = min(dp[i],dp[j] + dp[i-j]);
         }
-    }
+    }*/
 
-    if(dp[k] == INT_MAX)
+    if(dp[k] == INF)
         printf("-1\n");
     else
         printf("%d\n",dp[k]);
