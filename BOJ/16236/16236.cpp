10b5146dbdb2385495f4ae0f401bdc2f029ff79c
YongBon Jeon <bon6143@gmail.com>
2023-07-23 17:12:47 +0900

    BFS(우선순위 큐)

--- BOJ/16236/16236.cpp
+++ BOJ/16236/16236.cpp
@@ -4,42 +4,73 @@ using namespace std;
 int space[21][21];
 int N, shark_y, shark_x, shark_size;
 int target_y, target_x;
+int dy[4] = {-1,0,0,1};
+int dx[4] = {0,-1,1,0};
 
 int caneat(){
-    int fish = 0;
-    for(int i = 0 ; i < N ; i++){
-        for(int j = 0 ; j < N ; j++){
-            if(space[i][j] < shark_size && space[i][j] != 0)
-                fish++;
+    
+    queue<pair<int,int>> q;
+    int visited[21][21];
+    memset(visited,0,sizeof(visited));
+
+    q.push({shark_y,shark_x});
+    visited[shark_y][shark_x] = 1;
+
+    int cur_y,cur_x,next_y,next_x;
+    while(!q.empty()){
+        cur_y = q.front().first;
+        cur_x = q.front().second;
+        q.pop();
+        //printf("%d %d\n",cur_y,cur_x);
+
+        if(space[cur_y][cur_x] != 0 && space[cur_y][cur_x] < shark_size){
+            return 1;
         }
+
+        for(int i = 0 ; i < 4 ; i++){
+            next_y = cur_y + dy[i];
+            next_x = cur_x + dx[i];
+
+            //printf("%d %d %d\n",next_y,next_x,visited[next_y][next_x]);
+            if(next_y < 0 || next_x < 0 || next_y >= N || next_x >= N)
+                continue;
+            
+            if(space[next_y][next_x] == 0 || space[next_y][next_x] <= shark_size){
+                if(visited[next_y][next_x] == 0){
+                    visited[next_y][next_x] = visited[cur_y][cur_x] + 1;
+                    q.push({next_y,next_x});
+                }
+            }
+        }
+
     }
-    return fish;
+
+    return -1;
 }
 
 int bfs(){
-    int dy[4] = {-1,0,0,1};
-    int dx[4] = {0,-1,1,0};
     int visited[21][21];
 
     memset(visited,0,sizeof(visited));
 
-    priority_queue<pair<int,int>> q;
+    priority_queue<tuple<int,int,int>> q;
 
-    q.push({-shark_y, -shark_x});
+    q.push({-1,-shark_y, -shark_x});
     visited[shark_y][shark_x] = 1;
-    int cur_y, cur_x,next_y,next_x;
+    int cur_y, cur_x,next_y,next_x,val;
     while(!q.empty()){
-        cur_y = -q.top().first;
-        cur_x = -q.top().second;
+        tie(val,cur_y,cur_x) = q.top();
+        cur_y = -cur_y;
+        cur_x = -cur_x;
         q.pop();
 
-        printf("%d %d\n",cur_y,cur_x);
+        //printf("%d %d\n",cur_y,cur_x);
 
         if(space[cur_y][cur_x] < shark_size && space[cur_y][cur_x] != 0){
             target_y = cur_y;
             target_x = cur_x;
-            printf("%d %d %d\n",target_y,target_x, visited[cur_y][cur_x]);
-            return visited[cur_y][cur_x];
+            //printf("%d %d %d\n",target_y,target_x, visited[cur_y][cur_x]);
+            return visited[cur_y][cur_x]-1;
         }
 
         for(int i = 0 ; i < 4 ; i++){
@@ -52,7 +83,7 @@ int bfs(){
             if(space[next_y][next_x] <= shark_size){
                 if(visited[next_y][next_x] == 0){        
                     visited[next_y][next_x] += visited[cur_y][cur_x] + 1;
-                    q.push({-next_y,-next_x});
+                    q.push({-visited[next_y][next_x],-next_y,-next_x});
                 }
             }
         }
@@ -76,29 +107,32 @@ int main(){
 
     int totaleat = 0;
     int doeat, cnt = 0, dist;
-    int k=0;
-    while((doeat = caneat()) != 0 && (k++)<4){
-        printf("doeat %d shark_size %d cnt %d\n",doeat,shark_size,cnt);
+    while((doeat = caneat()) != -1){
+        /*printf("doeat %d shark_size %d cnt %d\n",doeat,shark_size,cnt);
         for(int i = 0 ; i < N ; i++){
             for(int j = 0 ; j < N ; j++){
                 printf("%d ",space[i][j]);
             }
             printf("\n");
-        }
+        }*/
         if(doeat == 1){
-            dist = bfs();
+            if((dist = bfs()) == -1){
+                break;
+            }
             space[shark_y][shark_x] = 0;
-            space[target_y][target_x] = 9;
+            space[target_y][target_x] = 0;
             cnt += dist;
             totaleat += 1;
             shark_y = target_y;
             shark_x = target_x;
         }
         else{
-            dist = bfs();
+            if((dist = bfs()) == -1){
+                break;
+            }
             cnt += dist;
             space[shark_y][shark_x] = 0;
-            space[target_y][target_x] = 9;
+            space[target_y][target_x] = 0;
             shark_y = target_y;
             shark_x = target_x;
             totaleat += 1;
