5c50223b884708cf1d9abae2daeeceba1ff616f9
YongBon Jeon <bon6143@gmail.com>
2024-01-22 21:22:56 +0900

    1/21 - 자료구조 활용 Hard

--- Programmers/2024 KAKAO WINTER INTERNSHIP - 3.java
+++ Programmers/2024 KAKAO WINTER INTERNSHIP - 3.java
@@ -11,6 +11,12 @@ class Solution {
         // 주사위를 N / 2 번 고르는 경우의 수
         List<Integer> list = new ArrayList<>();
         answer = new ArrayList<>();
+
+        List<Integer> test1 = new ArrayList<>();
+        List<Integer> test2 = new ArrayList<>();
+        test1.add(1); test1.add(2);
+        test2.add(3); test2.add(4);
+        //solve(test1, test2, dice);
         pick(N, 1, list, dice);
 
         int[] ans = new int[N/2];
@@ -44,7 +50,109 @@ class Solution {
     }
 
     public Integer solve(List<Integer> A, List<Integer> B, int[][] dice) {
-        /*System.out.println("A");
+
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> queue_new = new LinkedList<>();
+
+        queue.add(0);
+        for(int i = 0 ; i < A.size() ; i++) {
+            int size = queue.size();
+            for(int s = 0 ; s < size ; s++) {
+                int cur = queue.poll();
+
+                for(int j = 0 ; j < 6 ; j++) {
+                    queue.add(cur + dice[A.get(i)-1][j]);
+                }
+            }
+        }
+        int[] temp = new int[501];
+        while(!queue.isEmpty()) {
+            temp[queue.poll()] += 1;
+        }
+
+        int[] statusA = new int[501];
+
+        int sum = 0;
+        for(int i = 0 ; i < 501 ; i++) {
+            sum += temp[i];
+            statusA[i] = sum;
+        }
+
+        int ans = 0;
+
+        queue = new LinkedList<>();
+
+        queue.add(0);
+        for(int i = 0 ; i < B.size() ; i++) {
+            int size = queue.size();
+            for(int s = 0 ; s < size ; s++) {
+                int cur = queue.poll();
+
+                for(int j = 0 ; j < 6 ; j++) {
+                    queue.add(cur + dice[B.get(i)-1][j]);
+                }
+            }
+        }
+        temp = new int[501];
+        while(!queue.isEmpty()) {
+            temp[queue.poll()] += 1;
+        }
+        for(int i = 0 ; i < 501 ; i++) {
+            ans += statusA[i]*temp[i];
+        }
+        return ans;
+    }
+}
+
+/*
+import java.util.*;
+
+class Solution {
+    List<Integer> answer;
+    int min = 987654321;
+    public int[] solution(int[][] dice) {
+
+
+        int N = dice.length;
+
+        // 주사위를 N / 2 번 고르는 경우의 수
+        List<Integer> list = new ArrayList<>();
+        answer = new ArrayList<>();
+        pick(N, 1, list, dice);
+
+        int[] ans = new int[N/2];
+
+        for(int i = 0 ; i < answer.size() ; i++){
+            ans[i] = answer.get(i);
+        }
+
+        return ans;
+    }
+
+    public void pick(int N, int cur, List<Integer> list, int[][] dice) {
+        if(list.size() == N/2) {
+            List<Integer> B = new ArrayList<>();
+            for(int i = 1 ; i <= N ; i++)
+                if(!list.contains(i))
+                    B.add(i);
+
+            int temp;
+            if((temp = solve(list,B,dice)) < min){
+                min = temp;
+                answer = new ArrayList<>(list);
+            }
+        }
+
+        for(int i = cur ; i <= N ; i++) {
+            list.add(i);
+            pick(N, i+1, list, dice);
+            list.remove(Integer.valueOf(i));
+        }
+    }
+
+    public Integer solve(List<Integer> A, List<Integer> B, int[][] dice) {
+        */
+/*System.out.println("A");
         for(Integer a : A)
             System.out.print(a + " ");
         System.out.println();
@@ -52,7 +160,8 @@ class Solution {
         for(Integer b : B)
             System.out.print(b + " ");
         System.out.println();
-        */
+        *//*
+
 
 
         Map<Integer, Integer> num = new HashMap<>();
@@ -73,10 +182,12 @@ class Solution {
             num = num1;
         }
 
-        /*System.out.println("A 상태");
+        */
+/*System.out.println("A 상태");
         for (Map.Entry<Integer, Integer> entry : num.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
-        }*/
+        }*//*
+
 
         int[] statusA = new int[501];
 
@@ -85,11 +196,13 @@ class Solution {
             sum += num.getOrDefault(i,0);
             statusA[i] = sum;
         }
-        /*System.out.println("A 자료구조 ");
+        */
+/*System.out.println("A 자료구조 ");
         for(int i = 0 ; i <= 10 ; i++)
             System.out.println(i + " " + statusA[i] + " ");
         System.out.println();
-         */
+         *//*
+
         num = new HashMap<>();
         num1 = new HashMap<>();
         int ans = 0;
@@ -109,10 +222,12 @@ class Solution {
             num = num1;
         }
 
-        /*System.out.println("B 상태");
+        */
+/*System.out.println("B 상태");
         for(Integer b : B)
             System.out.print(b + " ");
-        System.out.println();*/
+        System.out.println();*//*
+
         for(Map.Entry<Integer, Integer> entry : num.entrySet()) {
             //System.out.println(entry.getKey() + " " + entry.getValue() + " " + statusA[entry.getKey()-1]);
             ans += statusA[entry.getKey()-1]*entry.getValue();
@@ -121,4 +236,4 @@ class Solution {
 
         return ans;
     }
-}
+}*/
